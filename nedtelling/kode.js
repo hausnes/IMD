2367c2fdb7ed506fa220d6c6dc52a8f2a7773ee3
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-09-04 21:40:48 +0200

    Nedtelling

---
+++ nedtelling/kode.js
@@ -0,0 +1,21 @@
+let antallSekunder = prompt("Skriv inn antall sekunder:");
+
+
+let nedtelling = setInterval(myTimer, 1000);
+
+function myTimer() {
+    antallSekunder = antallSekunder - 1;
+    document.getElementById("utskrift").innerText = antallSekunder;
+
+    if (antallSekunder <= 0) {
+        document.getElementById("utskrift").innerText = "Jippi!";
+        clearInterval(nedtelling);
+    }
+} 
+
+/*
+    Legge til endret farge når det nærmer seg.
+    Vere mulig å skrive inn dager, minutter ...
+    Lyder...
+    
+*/
