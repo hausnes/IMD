b98e6c503d2cb5a4ec8ba7582f9a428aa05d4a98
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-09-04 22:52:34 +0200

    Nedtelling (felles)

--- nedtelling/kode.js
+++ nedtelling/kode.js
@@ -1,12 +1,12 @@
 let antallSekunder = prompt("Skriv inn antall sekunder:");
-
+document.getElementById("utskrift").innerText = antallSekunder;
 
 let nedtelling = setInterval(myTimer, 1000);
 
 function myTimer() {
     antallSekunder = antallSekunder - 1;
     document.getElementById("utskrift").innerText = antallSekunder;
-
+    
     if (antallSekunder <= 0) {
         document.getElementById("utskrift").innerText = "Jippi!";
         clearInterval(nedtelling);
