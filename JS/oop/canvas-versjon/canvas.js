af576d714947efc4e1556f1d36f088d005ded198
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-12-02 17:10:20 +0100

    Update canvas.js

--- JS/oop/canvas-versjon/canvas.js
+++ JS/oop/canvas-versjon/canvas.js
@@ -2,6 +2,11 @@ let listeFiendar = [];
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const canvasBredde = 600;
+const canvasHogde = 600;
+
+let fartX = 1;
+let fartY = 1;
 
 class Karakter {
     constructor(
@@ -13,7 +18,11 @@ class Karakter {
         bilde, // eventuelt bilde av karakter, i staden for farge
         xPos,
         yPos,
-        lever // true/false
+        lever, // true/false
+        xretning, // om den går til høgre (positivt tall), eller venstre (neg.)
+        yretning, // om den går opp (neg) eller ned (pos)
+        xfart,
+        yfart
     ) {
         this.typeKarakter = typeKarakter,
         this.hp = hp,
@@ -23,13 +32,53 @@ class Karakter {
         this.bilde = bilde,
         this.xPos = xPos,
         this.yPos = yPos,
-        this.lever = lever
+        this.lever = lever,
+        this.xretning = 1,
+        this.yretning = 1
     }
 
     tegnKarakter() {
-        ctx.rect(this.xPos, this.yPos, this.bredde, this.hogde);
         ctx.fillStyle = this.farge;
-        ctx.fill();
+        ctx.fillRect(this.xPos, this.yPos, this.bredde, this.hogde);
+        
+        this.yPos = this.yPos + this.yfart * this.yretning;
+        this.xPos = this.xPos + this.xfart * this.xretning;
+        // ctx.fill();
+
+        // ball.xpos = ball.xpos + ball.xfart * ball.xretning; //Endrer x-posisjon 
+        // ball.ypos = ball.ypos + ball.yfart * ball.yretning; //Endrer y-posisjon
+        // racket.ypos = racket.ypos + racket.yfart * racket.yretning;
+    }
+
+    endreXpos(fartX) {
+        this.xPos = this.xPos + fartX * this.xretning;
+    }
+
+    endreYpos(fartY) {
+        this.yPos = this.yPos + fartY * this.yretning;
+    }
+
+    endreXretning(retning) {
+        this.xretning = retning;
+    }
+
+    endreYretning(retning) {
+        this.yretning = retning;
+    }
+
+    sjekkOmTreffVegg() {
+        if(this.xPos > (canvasBredde - this.bredde)) {
+            this.endreXretning(-1);
+        }
+        else {
+            this.endreXretning(1);
+        }
+        if(this.yPos > (canvasHogde - this.hogde)) {
+            this.endreYretning(-1);
+        }
+        else {
+            this.endreYretning(1);
+        }
     }
 
     // leggTilListe(karakter) {
@@ -48,10 +97,24 @@ class Fiende extends Karakter {
 }
 */
 
-let helten = new Karakter("helt",100,10,10,"green","",10,10,true);
+let helten =    new Karakter(  "helt",
+                                100,
+                                10,
+                                10,
+                                "green",
+                                "",
+                                10,
+                                10,
+                                true,
+                                1,
+                                1,
+                                1,
+                                1
+                );
 
 function gameLoop() {
     helten.tegnKarakter();
+    // helten.sjekkOmTreffVegg();
 
     requestAnimationFrame(gameLoop);
 }
