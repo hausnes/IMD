387dd55c7afefdf8a9c5f00e37859600aae4df34
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-12-02 08:47:31 +0100

    Update canvas.js

--- JS/oop/canvas-versjon/canvas.js
+++ JS/oop/canvas-versjon/canvas.js
@@ -1,25 +1,57 @@
-class karakter {
+let listeFiendar = [];
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+class Karakter {
     constructor(
         typeKarakter, // "helt" eller "fiende"
         hp,
         bredde, 
-        hogde, 
+        hogde,
+        farge, // bakgrunnsfarge for karakter 
         bilde, // eventuelt bilde av karakter, i staden for farge
         xPos,
         yPos,
-        farge,
         lever // true/false
-    )
+    ) {
+        this.typeKarakter = typeKarakter,
+        this.hp = hp,
+        this.bredde = bredde,
+        this.hogde = hogde,
+        this.farge = farge,
+        this.bilde = bilde,
+        this.xPos = xPos,
+        this.yPos = yPos,
+        this.lever = lever
+    }
+
+    tegnKarakter() {
+        ctx.rect(this.xPos, this.yPos, this.bredde, this.hogde);
+        ctx.fillStyle = this.farge;
+        ctx.fill();
+    }
+
+    // leggTilListe(karakter) {
+    //     listeFiendar.push(karakter);
+    // }
+    
 }
 
-class Fiende extends karakter {
+
+
+/*
+class Fiende extends Karakter {
     constructor() {
-        
+        super();
     }
 }
+*/
+
+let helten = new Karakter("helt",100,10,10,"green","",10,10,true);
 
 function gameLoop() {
-    tegnHelt();
+    helten.tegnKarakter();
 
     requestAnimationFrame(gameLoop);
 }
