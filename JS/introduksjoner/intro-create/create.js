8db4c9f21849719e4fce842e1cfba9ecb5dc56a2
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-02-03 11:04:23 +0100

    Update create.js

--- JS/introduksjoner/intro-create/create.js
+++ JS/introduksjoner/intro-create/create.js
@@ -7,3 +7,19 @@ bilde.src = "https://gfx.nrk.no/oRChumeWruhkPhW7TbEf5wgo9Di9zmAMeLdXsNAXUOcg";
 bilde.alt = "Bilde av inergionerign";
 bilde.id = "bilde";
 document.body.appendChild(bilde);
+
+// https://randomuser.me/api/?results=1&gender=female&nat=no
+
+let antall = 5;
+let kjonn = "female";
+let nasjonalitet = "no";
+
+fetch('https://randomuser.me/api/?results=' + antall + "&gender=" + kjonn + "&nat=" + nasjonalitet)
+	.then(response => response.json())
+	// .then(response => console.log(response))
+    .then(response => behandleSvar(response))
+	.catch(err => console.error(err));
+
+function behandleSvar(data) {
+    console.log(data.results[0].gender);
+}
