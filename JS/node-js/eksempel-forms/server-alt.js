8190d2275ec1fc0cceb241bfaabd1f97a22e5b41
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-02-21 09:30:04 +0100

    Test-kode

--- JS/node-js/eksempel-forms/server-alt.js
+++ JS/node-js/eksempel-forms/server-alt.js
@@ -12,10 +12,19 @@ let chatLogg = [
 function formHandler(req, res) {
     console.log("Innkommende body: " + JSON.stringify(req.body));
     console.log("Input-feltet message: " + req.body.message);
-    // chatLogg.push(req.body.message);
+    chatLogg.push(req.body.message);
     // const li = document.createElement('li');
     // li.innerHTML = req.body.message;
-    res.redirect('/form-alt.html');
+    // res.redirect('/form-alt.html');
+}
+
+function visChatLogg(req, res, chatLogg) {
+	let utskrift = "<ul>";
+	for(let data of chatLogg) {
+		console.log("<li>" + data.message + "</li>");
+	}
+	utskrift += "</ul>";
+	res.send(utskrift);
 }
 
 // Serve the HTML file with the form
@@ -30,11 +39,11 @@ app.use(express.static(publicDirectoryPath));
 // Handle form submission
 app.post('/sendInn', formHandler);
 
-// app.get('getData', (req, res) => {
-//     res.send(chatLogg);
-// });
+app.get('getData', (req, res) => {
+    res.send(chatLogg);
+});
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+	console.log('Server started on port 3000');
 });
