6c9c18469fe283821992ab66e6e418c462f8b809
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-03-27 20:58:42 +0200

    produkt-side

--- JS/node-js/nettbutikk-v1/public/produkter.js
+++ JS/node-js/nettbutikk-v1/public/produkter.js
@@ -4,6 +4,42 @@ async function visProdukter() {
     const url = await fetch("/produkter");
     const produkter = await url.json();
     console.log(produkter); // Test for å se om vi får hentet data
+
+    for (let vare of produkter) {
+        let div = document.createElement("div");
+        // div.setAttribute("class", "vare");
+        // div.class = "vare";
+
+        let overskrift = document.createElement("h1");
+        overskrift.innerText = vare.navn;
+
+        let bilde = document.createElement("img");
+        bilde.src = vare.bilde;
+
+        let beskrivelse = document.createElement("p");
+        beskrivelse.innerText = vare.beskrivelse;
+
+        let pris = document.createElement("p");
+        pris.innerText = vare.pris;
+
+        let knapp = document.createElement("button");
+        knapp.id = vare.id;
+        knapp.addEventListener("click", leggTilVare);
+        knapp.innerText = "Kjøp!";
+
+        div.appendChild(overskrift);
+        div.appendChild(bilde);
+        div.appendChild(beskrivelse);
+        div.appendChild(pris);
+        div.appendChild(knapp);
+
+        utskrift.appendChild(div);
+    }
 }
 
 visProdukter();
+
+function leggTilVare(event) {
+    console.log(event.target.id);
+    console.log(event.target);
+}
